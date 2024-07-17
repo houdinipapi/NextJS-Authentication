dde2d4d93c3d25321ef37d48875e9e7464ed4f3c
Houdini Papi <ochiengcliff.co@gmail.com>
2024-07-17 23:46:03 +0300

    feat: Add server-side session handling to Member page

--- authentication/src/app/Member/page.tsx
+++ authentication/src/app/Member/page.tsx
@@ -1,9 +1,23 @@
+import { getServerSession } from 'next-auth';
 import React from 'react'
+import options from '../api/auth/[...nextauth]/options';
+import { redirect } from 'next/navigation';
 
-const MemberPage = () => {
+const MemberPage = async () => {
+
+  const session: { user?: { email?: string, role?: string } } | null = await getServerSession(options)
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/Member");
+  }
+
+
   return (
     <div>
         <h1>Member Page</h1>
+        <p>{session?.user?.email}</p>
+        <p>{session?.user?.role}</p>
+
     </div>
   )
 }
