5b0c38ee744a9772ca37859c53b22bba3ae8d9fe
Houdini Papi <ochiengcliff.co@gmail.com>
2024-07-17 20:41:30 +0300

    chore: Update Next.js dependencies and add authentication providers

--- authentication/src/app/api/auth/[...nextauth]/options.tsx
+++ authentication/src/app/api/auth/[...nextauth]/options.tsx
@@ -1,6 +1,66 @@
 import React from 'react'
+import NextAuth from "next-auth"
+import GitHubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
 
 export const options = {
-    
-}
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID ?? '',
+            clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
+
+            profile(profile) {
+                console.log("GitHub Profle: ", profile)
+
+                let userRole = "GitHub User"
+                if (profile?.email === "olivertim.ot@gmail.com"){
+                    userRole = "admin";
+                }
+
+                return {
+                    ...profile,
+                    id: profile.id.toString(),
+                    role: userRole,
+                };
+            },
+        }),
+
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        
+            profile(profile) {
+                console.log("Google Profle: ", profile)
+        
+                let userRole = "Google User"
+                // if (profile?.email == "olivertim.ot@gmail.com"){
+                //     userRole = "admin";
+                // }
+        
+                return {
+                    ...profile,
+                    id: profile.sub,
+                    role: userRole,
+                };
+            },
+        }),
+    ],
+    callbacks: {
+        async jwt({token, user}: {token: any, user: any}) {
+            if (user) {
+                token.id = user.id;
+                token.role = user.role;
+            }
+            return token;
+        },
+        async session({session, token}: {session: any, token: any}) {
+            session.id = token.id;
+            session.role = token.role;
+            return session;
+        },
+    }
+};
+
+
+export default NextAuth(options);
